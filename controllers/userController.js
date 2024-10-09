392b3d2cd1959f48ea510fcf6faeb944111c7d21
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-10-09 11:34:01 +1100

    CRUD operations added, also added friend management function and reaction management

---
+++ controllers/userController.js
@@ -0,0 +1,46 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Get a single user by ID
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .populate('thoughts')
+      .populate('friends')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Update a user by ID
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
+
