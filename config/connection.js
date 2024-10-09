7a99925ddf8644716a9e5d35869382011109b428
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-10-09 12:43:11 +1100

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork', {
-  useNewUrlParser: true, // Use the new URL parser
-  useUnifiedTopology: true, // Use the new server discovery and monitoring engine
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork');
 
 // Export the connection
 module.exports = mongoose.connection;
